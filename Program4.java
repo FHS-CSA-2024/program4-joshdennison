0b630bb78487e67fcb98417db619a90d91212719
joshdennison <jden3.14@gmail.com>
2024-08-19 09:17:27 -0700

    Update Program4.java

--- Program4.java
+++ Program4.java
@@ -1,6 +1,29 @@
 //Youre on your own!!
 //Jk ask for help if you need it (but also look at previous programs!!)
-
+import java. util. Scanner;
+public class main
+{
+    public static void main(String[] args)
+    {
+        Scanner num1 = new Scanner(System.in);
+        Scanner num2 = new Scanner(System.in);
+        Scanner num3 = new Scanner(System.in);
+        Scanner num4 = new Scanner(System.in);
+        System.out.print("enter first number");
+        int Num1 = num1.nextInt();
+         System.out.print("enter second number");
+        int Num2 = num2.nextInt();
+         System.out.print("enter third number");
+        int Num3 = num3.nextInt();
+         System.out.print("enter fourth number");
+        int Num4 = num4.nextInt();
+        int sum = (Num1+Num2+Num3+Num4);
+        System.out.print("the sum is  "+ sum);
+        double avg = (double) sum/4;
+        System.out.print( "the average is " + avg);
+        
+    }
+}
 
 
 //Paste console output below:
